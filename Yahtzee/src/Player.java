fd5fa41a9c955c94d69133e664d75584d2a9b5a8
Siyu <59789515+TROY571@users.noreply.github.com>
2021-02-25 11:31:09 +0100

    Update Player.java

--- Yahtzee/src/Player.java
+++ Yahtzee/src/Player.java
@@ -1,10 +1,29 @@
 public class Player
 {
   private Round round;
-  private int score;
+  private String name;
+
+  public int[] round()
+  {
+    return 
+  }
+
+  public boolean getAdditionalYahtzeeScore()
+  {
+    return false;
+  }
 
   public int finalScore()
   {
-    return 
+    int count=0;
+    if (round.sum()>63)
+    {
+      count = round.sum()+ 35;
+    }
+    else
+    {
+      count = round.sum();
+    }
+    return count;
   }
 }
