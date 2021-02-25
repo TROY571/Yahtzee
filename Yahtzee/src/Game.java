4acbec103691cf720de1c4813f773cabe394ad09
Lokkaze <74102422+Lokkaze@users.noreply.github.com>
2021-02-25 11:40:37 +0100

    Update Game.java

--- Yahtzee/src/Game.java
+++ Yahtzee/src/Game.java
@@ -10,4 +10,25 @@ public class Game
   {
     if(player1.)
   }
+  
+  public String printScoreBoard()
+  {
+    String board = "";
+    board += "aces:" + "\n";
+    board += "twos:" + "\n";
+    board += "threes:" + "\n";
+    board += "fours:" + "\n";
+    board += "fives:" + "\n";
+    board += "sixes:" + "\n";
+    board += "3 of kind:" + "\n";
+    board += "4 of kind:" + "\n";
+    board += "yahtzee:" + "\n";
+    board += "full house:" + "\n";
+    board += "small straight:" + "\n";
+    board += "large straight:" + "\n";
+    board += "sum:" + "\n";
+    board += "total bonus:" + "\n";
+    return board;
+  }
+  
 }
